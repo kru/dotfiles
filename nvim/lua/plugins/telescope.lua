596a94ab897405b351c28a27fb5241441f5a7388
kris <kristoforusrp@gmail.com>
2024-12-11 12:13:13 +0700

    multi grep of telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,3 +1,49 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local make_entry = require("telescope.make_entry")
+local conf = require("telescope.config").values
+
+local live_multigrep = function(opts)
+	opts = opts or {}
+	opts.cwd = opts.cwd or vim.uv.cwd()
+
+	local finder = finders.new_async_job {
+		command_generator = function(prompt)
+			if not prompt or prompt == "" then
+				return nil
+			end
+
+			local pieces = vim.split(prompt, "  ")
+			local args = { "rg" }
+			if pieces[1] then
+				table.insert(args, "-e")
+				table.insert(args, pieces[1])
+			end
+
+			if pieces[2] then
+				table.insert(args, "-g")
+				table.insert(args, pieces[2])
+			end
+
+			--@diagnostic disable-next-line: deprecated
+			return vim.tbl_flatten {
+				args,
+				{ "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+			}
+		end,
+		entry_maker = make_entry.gen_from_vimgrep(opts),
+		cwd = opts.cwd,
+	}
+
+	pickers.new(opts, {
+		debounce = 100,
+		prompt_title = "Live Multi Grep",
+		finder = finder,
+		previewer = conf.grep_previewer(opts),
+		sorter = require("telescope.sorters").empty(),
+	}):find()
+end
+
 return {
 	{
 
@@ -45,11 +91,12 @@ return {
 		config = function(_, opts)
 			local builtin = require("telescope.builtin")
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-			vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+			vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
+			vim.keymap.set('n', '<leader>fw', live_multigrep, {})
 			vim.keymap.set('n', '<leader>ls', builtin.oldfiles,
 				{ desc = '[?] Find recently opened files' })
-			vim.api.nvim_set_keymap("n", "<leader>lf", ":lua require('telescope.builtin').lsp_document_symbols({ symbols='function' })<CR>",
+			vim.api.nvim_set_keymap("n", "<leader>lf",
+				":lua require('telescope.builtin').lsp_document_symbols({ symbols='function' })<CR>",
 				{ noremap = true })
 			vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 			require('telescope').setup(opts)
