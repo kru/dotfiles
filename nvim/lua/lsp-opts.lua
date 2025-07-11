c7c54f76295e9b235b2db801cf21d3070218eef7
kris-rp <kristoforus.pamungkas@tekuton.id>
2025-07-11 08:58:54 +0700

    update to neovim 0.11 config

--- nvim/lua/lsp-opts.lua
+++ nvim/lua/lsp-opts.lua
@@ -1,3 +1,6 @@
+-- LSP Stuff
+local lspconfig = require('lspconfig')
+
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
@@ -9,7 +12,7 @@ local on_attach = function(_, bufnr)
 	-- for LSP related items. It sets the mode, buffer and description for us each time.
 	-- Enable completion triggered by <c-x><c-o>
 	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	-- local bufopts = { noremap = true, silent = true, buffer = bufnr }
 	local nmap = function(keys, func, desc)
 		if desc then
 			desc = "LSP: " .. desc
@@ -21,12 +24,12 @@ local on_attach = function(_, bufnr)
 	-- nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-	nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-	nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-	nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+	nmap("gd", "<cmd>lua vim.lsp.buf.definition()<CR>", "[G]oto Definition")
+	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto References")
+	nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto Implementation")
+	nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type Definition")
+	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument Symbols")
+	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace Symbols")
 
 	-- See `:help K` for why this keymap
 	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
@@ -38,178 +41,73 @@ local on_attach = function(_, bufnr)
 	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
 	nmap("<leader>wl", function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, "[W]orkspace [L]ist Folders")
+	end, "[W]orkspace List Folders")
 
 	vim.keymap.set("n", "<leader>fm", function()
 		vim.lsp.buf.format()
 	end, { buffer = bufnr, desc = "format current file" })
 end
 
--- LSP Stuff
-require("mason").setup()
-require("mason-lspconfig").setup()
+-- auto completion
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+		on_attach(client, vim.api.nvim_get_current_buf())
+	end,
+})
 
--- local util = require "lspconfig/util",
-local servers = {
-	clangd = {
-		max_len_aligh = false,
+-- Define LSP configurations
+local configs = {
+	ts_ls = {
+		capabilities = require('cmp_nvim_lsp').default_capabilities(),
+		on_attach = on_attach,
+		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
 	},
 	gopls = {
+		capabilities = require('cmp_nvim_lsp').default_capabilities(),
+		on_attach = on_attach,
+		filetypes = { "go", "gomod", "gowork", "gotmpl" },
 		settings = {
-			completeUnimported = true,
-			usePlaceholders = true,
-			gofumpt = true,
-			staticcheck = true,
-			analyses = {
-				unusedparams = true,
+			gopls = {
+				analyses = {
+					unusedparams = true,
+				},
+				staticcheck = true,
 			},
 		},
 	},
-	-- pyright = {},
-	-- rust_analyzer = {},
-	ts_ls = {
-		filetypes = { "typescript", "javascript" },
-	},
-	zls = {
-		filetypes = { "zig", "zir" },
-	},
-	-- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
 	lua_ls = {
-		Lua = {
-			runtime = { version = "LuaJIT" },
-			workspace = { checkThirdParty = false },
-			telemetry = { enable = false },
-			completion = { callSnippet = "Replace" },
-			-- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-			-- diagnostics = { disable = { 'missing-fields' } },
-		},
-	},
-}
--- Setup neovim lua configuration
-require("neodev").setup()
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
--- Ensure the servers above are installed
-local mason_lspconfig = require("mason-lspconfig")
-
-mason_lspconfig.setup({
-	ensure_installed = vim.tbl_keys(servers),
-})
-
-mason_lspconfig.setup_handlers({
-	function(server_name)
-		require("lspconfig")[server_name].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = servers[server_name],
-			filetypes = (servers[server_name] or {}).filetypes,
-		})
-	end,
-})
-
-capabilities.textDocument.completion.completionItem = {
-	documentationFormat = { "markdown", "plaintext" },
-	snippetSupport = true,
-	preselectSupport = true,
-	insertReplaceSupport = true,
-	labelDetailsSupport = true,
-	deprecatedSupport = true,
-	commitCharactersSupport = true,
-	tagSupport = { valueSet = { 1 } },
-	resolveSupport = {
-		properties = {
-			"documentation",
-			"detail",
-			"additionalTextEdits",
+		cmd = { 'lua-language-server' },
+		capabilities = require('cmp_nvim_lsp').default_capabilities(),
+		on_attach = on_attach,
+		filetypes = { "lua" },
+		root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = { 'vim' }, -- Recognize Neovim globals
+				},
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true), -- Neovim runtime files
+				},
+				runtime = {
+					version = 'LuaJIT',
+				}
+			},
 		},
 	},
 }
 
--- [[ Configure nvim-cmp ]]
--- See `:help cmp`
-local cmp = require("cmp")
-local luasnip = require("luasnip")
-require("luasnip.loaders.from_vscode").lazy_load()
-luasnip.config.setup({})
-
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	completion = {
-		completeopt = "menu,menuone,noinsert",
-	},
-	mapping = cmp.mapping.preset.insert({
-		-- ['<C-n>'] = cmp.mapping.select_next_item(),
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete({}),
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.locally_jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	}),
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-		{ name = "path" },
-	},
-})
-
--- organizeImports of go file
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.go",
-	callback = function()
-		local params = vim.lsp.util.make_range_params()
-		params.context = { only = { "source.organizeImports" } }
-		-- buf_request_sync defaults to a 1000ms timeout. Depending on your
-		-- machine and codebase, you may want longer. Add an additional
-		-- argument after params if you find that you have to write the file
-		-- twice for changes to be saved.
-		-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-		for cid, res in pairs(result or {}) do
-			for _, r in pairs(res.result or {}) do
-				if r.edit then
-					local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-					vim.lsp.util.apply_workspace_edit(r.edit, enc)
-				end
-			end
-		end
-		vim.lsp.buf.format({ async = false })
-	end,
-})
-
-local enable_provider = {
-	"node_provider",
-}
-
-for _, plugin in pairs(enable_provider) do
-	vim.g["loaded_" .. plugin] = nil
-	vim.cmd("runtime " .. plugin)
+-- Apply configurations to LSP servers
+for server, config in pairs(configs) do
+	lspconfig[server].setup(config)
 end
+
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("gopls")
+
+vim.lsp.buf.hover()
